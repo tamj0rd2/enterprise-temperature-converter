e868e001733b85152f4d5ca1374c76e7c13ecf3f
Tamara Jordan <tamara.jordan1+coding@hotmail.com>
2022-08-09 14:43:32 +0100

    Move a thing around

---
+++ acceptance/cli_driver.go
@@ -0,0 +1,50 @@
+package acceptance
+
+import (
+	"bytes"
+	"github.com/alecthomas/assert/v2"
+	"os"
+	"os/exec"
+	"path"
+	"strconv"
+	"testing"
+)
+
+type cliConverterDriver struct {
+	binaryPath string
+}
+
+func newCliConverterDriver(tb testing.TB) *cliConverterDriver {
+	dir, err := os.MkdirTemp("", "converter-*")
+	assert.NoError(tb, err)
+	tb.Cleanup(func() {
+		_ = os.RemoveAll(dir)
+	})
+	binaryPath := path.Join(dir, "converter-cli")
+	tb.Log(binaryPath)
+
+	b, err := exec.Command("go", "build", "-o", binaryPath, "../cmd/converter-cli/...").CombinedOutput()
+	output := string(b)
+	assert.NoError(tb, err, output)
+
+	return &cliConverterDriver{binaryPath: binaryPath}
+}
+
+func (d cliConverterDriver) FromFToC(i int) (int, error) {
+	var buf bytes.Buffer
+
+	cmd := exec.Command(d.binaryPath)
+	cmd.Stdout = &buf
+
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+
+	output := buf.String()
+	c, err := strconv.Atoi(output)
+	if err != nil {
+		panic(err)
+	}
+
+	return c, nil
+}
